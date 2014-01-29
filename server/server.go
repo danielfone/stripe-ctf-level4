4d149be4e789e4c688bfdfb811c452fa7167aab1
Daniel Fone <daniel@fone.net.nz>
2014-01-29 22:08:22 +1300

    Less sleep

--- server/server.go
+++ server/server.go
@@ -155,10 +155,11 @@ func (s *Server) Join(leader string) error {
 	}
 
 	for {
-    time.Sleep(time.Duration(rand.Intn(500)+100) * time.Millisecond)
+    //time.Sleep(time.Duration(rand.Intn(500)+100) * time.Millisecond)
 		_, err := s.client.SafePost(cs, "/join", b)
 		if err != nil {
 			log.Printf("Unable to join cluster: %s", err)
+      time.Sleep(1 * time.Second)
 			continue
 		}
     log.Println("Successfully joined cluster")
